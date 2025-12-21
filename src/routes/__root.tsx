81c2cfe9e555b5d668395be29a79f6b58e5c4a31
Thanakorn Phuttharaksa <crossdeltavi@gmail.com>
2025-12-21 10:27:52 +0100

    added project template scaffolding with mui and tanstack

---
+++ src/routes/__root.tsx
@@ -0,0 +1,41 @@
+import {
+  HeadContent,
+  Outlet,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import type { QueryClient } from '@tanstack/react-query'
+import TanStackQueryDevtools from '@/integrations/tanstack-query/devtools'
+import { Container, CssBaseline, ThemeProvider } from '@mui/material'
+import { MUI_THEME } from '@/theme'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  component: () => (
+    <>
+      <HeadContent />
+      <ThemeProvider theme={MUI_THEME}>
+        <CssBaseline />
+        <Container maxWidth="md" sx={{ paddingY: 4 }}>
+          <Outlet />
+        </Container>
+      </ThemeProvider>
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+          TanStackQueryDevtools,
+        ]}
+      />
+    </>
+  ),
+})
